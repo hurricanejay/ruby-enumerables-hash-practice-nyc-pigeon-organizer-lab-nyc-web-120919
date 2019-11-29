48a8680c2fb9097a098df984d112d70eb0295434
Jay Wen <jaywen89@gmail.com>
2019-11-29 17:59:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,9 @@ def nyc_pigeon_organizer(data)
   # write your code here!
 new_hash = {}
 data.each do |k,v| 
-  v.each
+  v.each do |new_value, names|
+    names.each do |name|
+      
+      if !new_hash[name]
+        new_hash[name] = {}
 end
